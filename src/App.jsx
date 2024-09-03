c50bb5863014f4b788b010320ab8986585820a4c
Ryan@Tenxrs <mpenyanaryan@gmail.com>
2024-09-03 22:51:31 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -3,91 +3,125 @@ import "./App.css";
 import { useState } from "react";
 import Header from "./components/Header";
 import { AppContext, ForecastContext, ThemeContext } from "./Context/Context";
-import WeatherContainer from "./WeatherContainer";
-import ForecastContainer from "./ForecastContainer";
-export const API_KEY = "c39dcb6fbf59d39634cb90bb49d9e560";
-// const API = process.env.API_KEY
-// .(API)
+import WeatherContainer from "./components/WeatherContainer";
+import ForecastContainer from "./components/ForecastContainer";
+import { API_KEY } from "./public/API";
 
 function App() {
   const [search, setSearch] = useState("Cape Town");
   const [data, setWeatherData] = useState();
   const [forecastData, setForecastData] = useState();
-  const [location, setLocation] = useState();     
-  const [theme , setTheme ] = useState(false)  
-  
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const MSRefreshTime = 300000;
   const themeHandler = () => {
-        var element = document.getElementById('root');
-        if(!theme){
-            setTheme(true)
-            element.style.backgroundImage = 'linear-gradient(to left top ,rgba(0, 0, 0, 0.979),rgba(27, 27, 27, 0.849) , rgba(27, 27, 27, 0.849)  )'
-            element.style.color = 'white'
-        }
-        else {
-            element.style.backgroundImage = 'linear-gradient(to left top ,rgba(0, 0, 0, 0.979) ,rgba(255, 255, 255, 1)   )'
-            element.style.color = 'black'
-            setTheme(false)
-        }
-  }
-  
-  
-  const handleSearch = async () => {
-    //geolocation data
-    try{
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search},&limit=5&appid=${API_KEY}&units=metric`)
-        const data = await response.json()
-        const results = data.filter((item, index) => item)
-        setLocation(results)
-    }catch(err){
-        console.log(err)
+    var element = document.getElementById('root');
+    if (isDarkMode) {
+      element.style.backgroundImage = 'linear-gradient(to left top ,rgba(0, 0, 0, 0.979),rgba(27, 27, 27, 0.849) , rgba(27, 27, 27, 0.849)  )'
+      element.style.color = 'white'
+      setIsDarkMode(false);
     }
+    else {
+      element.style.backgroundImage = 'linear-gradient(to left top ,rgba(0, 0, 0, 0.979) ,rgba(255, 255, 255, 1)   )'
+      element.style.color = 'black'
+      setIsDarkMode(true);
+    }
+  }
+
+  const handleSearch = async () => {
+    //current weather Data for searched location.
+    getCurrentWeather();
+  };
+
+
+  const getCurrentWeather = async (location) => {
     //current weather Data
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}&units=metric`)
-        const data = await response.json()
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location? location : search}&appid=${API_KEY}&units=metric`)
+      let data = await response.json()
+      if(data.cod != '404') {
         setWeatherData(data)
-        } catch(e){
-            console.log(e)
-        }       
-  
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    // getting data for hourly forecast
+    getForecastWeatherData();
+  }
+
+  const getInitialWeatherData = async () => {
     //Forecast data
+    getForecastWeatherData();
+  }
+
+  const getForecastWeatherData = async (location) => {
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${API_KEY}&units=metric`)
-      const data = await response.json()
-      setForecastData(data)
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location? location : search}&appid=${API_KEY}&units=metric`)
+      let data = await response.json()
+      //handling if place not found;
+      if(data.cod != '404') {
+        setForecastData(data)
+      }
     } catch (e) {
       console.log(e);
     }
-  };
+  }
+
+
+  const loadDefaultTheme = () => {
+    var element = document.getElementById('root');
+    element.style.backgroundImage = 'linear-gradient(to left top ,rgba(0, 0, 0, 0.979),rgba(27, 27, 27, 0.849) , rgba(27, 27, 27, 0.849)  )'
+    element.style.color = 'white'
+  }
 
   useEffect(() => {
+    getInitialWeatherData();
+    getCurrentWeather();
     handleSearch();
-    themeHandler()
+    loadDefaultTheme();
+    
+    let currentWeatherInterval = setInterval(() => getCurrentWeather(localStorage.getItem("currentLocation")), MSRefreshTime);
+    let forecastWeatherInterval = setInterval(() => getForecastWeatherData(localStorage.getItem("currentLocation")), MSRefreshTime);
+
+    return () => {
+      clearInterval(currentWeatherInterval);
+      clearInterval(forecastWeatherInterval);
+    }
   }, []);
 
   return (
-      <AppContext.Provider value={data}>
-        <Header
-          onSubmit={(e) => {
-              e.preventDefault();
+    data &&
+    <AppContext.Provider value={data}>
+      <Header
+        onSubmit={(e) => {
+          if (search.length > 0) {
+            e.preventDefault();
             handleSearch()
             setSearch('')
-          }}
-          onClick={handleSearch}
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-          handleTheme={themeHandler}
-        />
-        <ForecastContext.Provider value={forecastData}>
-      <div className={`App text-center gap-4 ${theme ? 'text-white' : 'text-black'}  pb-16 h-full  px-[20px] xl:px-5`}>
-        <ThemeContext.Provider value={theme}>
-          <WeatherContainer />
-          <ForecastContainer />
+          } else {
+            alert("Please provide valid input");
+          }
+        }}
+        onClick={handleSearch}
+        onChange={(e) => {
+          const searchTerm = e.target.value;
+          setSearch(searchTerm);
+          localStorage.setItem("currentLocation", searchTerm);
+        }}
+        value={search}
+        handleTheme={themeHandler}
+      />
+      {!errorMessage &&
+      <ForecastContext.Provider value={forecastData}>
+        <div className={`App text-center gap-4 ${isDarkMode ? 'text-black' : 'text-white'}  pb-16 h-full  px-[20px] xl:px-5`}>
+          <ThemeContext.Provider value={isDarkMode}>
+            <WeatherContainer />
+            <ForecastContainer />
           </ThemeContext.Provider>
-      </div>
-        </ForecastContext.Provider>
+        </div>
+      </ForecastContext.Provider>}
     </AppContext.Provider>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/DailyForecast.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import cloudIcon from "../assets/WeatherWhiteIcons/clouds.png";
+import { useContext } from "react";
+import { ForecastContext } from "../Context/Context";
+
+const DailyForecast = ({ item }) => {
+  const data = useContext(ForecastContext);
+  let dailyWeatherData = [];
+  if (data) {
+    let currentTimeStamp;
+    let today = new Date().toLocaleDateString();
+    for (let i = 0; i < data.list.length; i++) {
+      let item = data.list[i];
+      let dt_txt = item.dt_txt.split(" ")[0];
+      if (
+        currentTimeStamp != dt_txt &&
+        today != new Date(dt_txt).toLocaleDateString()
+      ) {
+        currentTimeStamp = dt_txt;
+        dailyWeatherData.push(item);
+      }
+    }
+  }
+  return (
+    <div className="flex flex-col px-6 py-4 mb-7 items-center  rounded-[30px] shadow-md lg:w-2/5  xl:w-[414px] xl:py-8 lg:h-[296px] xl:h-[366px]">
+      <h2 className="font-bold lg:text-xl xl:text-[32px] ">5 Days Forecast:</h2>
+      <div className="lg:flex  lg:gap-4 xl:gap-0 lg:flex-col justify-center">
+        {dailyWeatherData.length ? (
+          dailyWeatherData.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="flex items-center xl:justify-between xl:w-[370px] "
+              >
+                <img
+                  className=" lg:w-8 xl:w-[60px]"
+                  src={`https://openweathermap.org/img/wn/${
+                    item && item.weather[0].icon
+                  }@2x.png`}
+                  alt=""
+                />
+                <p className="xl:text-2xl font-semibold">
+                  {parseInt(item.main.temp)}°C
+                </p>
+                <div className="w-[163px] font-semibold">
+                  <p>{new Date(item.dt_txt).toDateString()}</p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h1>Fetching data please wait....</h1>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DailyForecast;
